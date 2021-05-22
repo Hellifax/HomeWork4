438c0a56c42363ed418e6ecca71f281d187daab7
Hellifax <hellifax91@gmail.com>
2021-05-22 22:57:57 +0300

    Homework 4

---
+++ HW4.1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HW4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+             Console.WriteLine(GetFullName());
+            }
+            
+        }
+        static string GetFullName()
+        {
+            Console.WriteLine("Enter name:");
+            string firtstName = Console.ReadLine();
+            Console.WriteLine("Enter surname:");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter patronymic:");
+            string patronymic = Console.ReadLine();
+            return $"{lastName} {firtstName} {patronymic}";
+                        
+
+        }
+
+    }
+}
---
+++ HW4.2/HW4.2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HW4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 2 числа:"); 
+            SumOfNumbers(Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())); 
+            //SumOfNumbers(20, 30); // также данные можно передать и таким образом
+        }
+        static int SumOfNumbers(int a, int b)
+        {
+            int result = a + b;            
+            Console.WriteLine(result);
+            return result;
+
+        }
+
+
+    }
+}
